14d66ea734cb4dca23256f525851acb1294676dd
Ainsley <aidan.king1999@hotmil.co.uk>
2014-03-04 14:54:33 +0000

    Create ascii.py

---
+++ ascii.py
@@ -0,0 +1,14 @@
+print ('a' in 'boat')
+#expected outcome is a
+print ('c' in 'boat')
+#expected outcome is *error*
+print('2' + '2')
+#expected outcome is *error*
+print (4*'4')
+#expected outcome is 16
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(len(alphabet))
+#expevted outcome is alphabet
+print(alphabet[1])
+#expected outcome is a
+print (aplhabet[0:6:2]
